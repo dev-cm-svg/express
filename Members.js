42ad394c1488d0de56845c2e72e0f07db9f60246
dev-cm-svg <email@example.com>
2020-08-08 11:51:14 -0400

    init

---
+++ Members.js
@@ -0,0 +1,8 @@
+const members = [
+  { id: 1, name: "John", email: "john@gmail.com", status: "active" },
+  { id: 2, name: "Smith", email: "smith@gmail.com", status: "active" },
+  { id: 3, name: "Sam", email: "sam@gmail.com", status: "active" },
+  { id: 4, name: "Jim", email: "jim@gmail.com", status: "inactive" },
+  { id: 5, name: "John", email: "john@gmail.com", status: "active" },
+];
+module.exports = members;
